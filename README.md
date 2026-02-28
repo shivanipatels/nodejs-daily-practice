6dbbf6ed4095c6ab6c39632fd5c8c98792d3f7bb
shivani patel <shivanipatels0123@gmail.com>
2026-02-28 17:06:51 +0530

    Day 2 - Created HTTP server with basic routing

--- README.md
+++ README.md
@@ -7,4 +7,11 @@ This repository contains my daily Node.js practice code.
 - Learned about asynchronous file writing
 - Implemented basic error handling
 
+## Day 2
+- Created HTTP server using http module
+- Implemented basic routing
+- Handled different URLs
+- Returned HTML responses
+
+
 More updates coming daily 💪
---
+++ day2-http-routing.js
@@ -0,0 +1,34 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Home Page</title></head>");
+    res.write("<body><h1>Home Page</h1></body>");
+    res.write("</html>");
+   return res.end();
+  } else if (req.url === "/products") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Products Page</title></head>");
+    res.write("<body><h1>Products Page</h1></body>");
+    res.write("</html>");
+   return res.end();
+  } else {
+     res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Page Not Found</title></head>");
+    res.write("<body><h1>Page Not Found</h1></body>");
+    res.write("</html>");
+   return res.end();
+  }
+
+  // process.exit();
+});
+
+const  PORT = 3000;
+server.listen(PORT, () => {
+  console.log(` Server is running on port http://localhost:${PORT}`);
+});
